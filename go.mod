af620ae197be3856468ab089f47e7f0a9851ee98
Terry Tay <txrrythk@gmail.com>
2021-12-11 18:04:19 +0800

    refactor todo domain with validations

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/terrytay/godo
 
 go 1.17
+
+require github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
--- internal/todo.go
+++ internal/todo.go
@@ -1,13 +1,10 @@
 package internal
 
 import (
-	"errors"
 	"time"
-)
 
-var ErrInvalidPriority = errors.New("parsed unknown value")
-var ErrInvalidStartAndDueDates = errors.New("start dates should be before end dates")
-var ErrDescriptionEmpty = errors.New("Task description cannot be empty")
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 const (
 	// PriorityNone indicates the task needs to be prioritized
@@ -28,7 +25,7 @@ func (p Priority) Validate() error {
 	case PriorityNone, PriorityLow, PriorityMedium, PriorityHigh:
 		return nil
 	}
-	return ErrInvalidPriority
+	return NewErrorf(ErrorCodeInvalidArgument, "unknown value")
 }
 
 // Category is used to organize the tasks
@@ -43,7 +40,7 @@ type Dates struct {
 // Validation validates the start and due date to be practical/legit
 func (d Dates) Validate() error {
 	if !d.Start.IsZero() && !d.Due.IsZero() && d.Start.After(d.Due) {
-		return ErrInvalidStartAndDueDates
+		return NewErrorf(ErrorCodeInvalidArgument, "start date should be before due date")
 	}
 	return nil
 }
@@ -61,16 +58,12 @@ type Task struct {
 
 // Validate validates fields of Task struct
 func (t Task) Validate() error {
-	if t.Description == "" {
-		return ErrDescriptionEmpty
-	}
-
-	if err := t.Priority.Validate(); err != nil {
-		return err
-	}
-
-	if err := t.Dates.Validate(); err != nil {
-		return err
+	if err := validation.ValidateStruct(&t,
+		validation.Field(&t.Description, validation.Required),
+		validation.Field(&t.Priority),
+		validation.Field(&t.Dates),
+	); err != nil {
+		return WrapErrorf(err, ErrorCodeInvalidArgument, "invalid values")
 	}
 
 	return nil
