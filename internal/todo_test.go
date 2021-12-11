d434ea92f86f7cf891dd3ed6204afc2624e68a9a
Terry Tay <txrrythk@gmail.com>
2021-12-11 16:57:25 +0800

    add test for Task

--- internal/todo_test.go
+++ internal/todo_test.go
@@ -111,3 +111,74 @@ func TestDates_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestTask_Validate(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		input   internal.Task
+		withErr bool
+	}{
+		{
+			"OK",
+			internal.Task{
+				Description: "eat my poopoo",
+				Dates: internal.Dates{
+					Start: time.Now(),
+					Due:   time.Now().Add(2 * time.Hour),
+				},
+				Priority: internal.PriorityHigh,
+			},
+			false,
+		},
+		{
+			"ERR: Description",
+			internal.Task{
+				Dates: internal.Dates{
+					Start: time.Now(),
+					Due:   time.Now().Add(2 * time.Hour),
+				},
+				Priority: internal.PriorityHigh,
+			},
+			true,
+		},
+		{
+			"ERR: Dates",
+			internal.Task{
+				Description: "eat my poopoo again",
+				Dates: internal.Dates{
+					Start: time.Now().Add(2 * time.Hour),
+					Due:   time.Now(),
+				},
+				Priority: internal.PriorityHigh,
+			},
+			true,
+		},
+		{
+			"ERR: Priority",
+			internal.Task{
+				Description: "eat my poopoo",
+				Dates: internal.Dates{
+					Start: time.Now(),
+					Due:   time.Now().Add(2 * time.Hour),
+				},
+				Priority: internal.Priority(-1),
+			},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			actualErr := tt.input.Validate()
+			if (actualErr != nil) != tt.withErr {
+				t.Fatalf("expected error %t, got %s", tt.withErr, actualErr)
+			}
+		})
+	}
+}
